0eaf0535aaed4a098e0063284d36f2cddef1b146
suhunlin <lin.suhun@msa.hinet.net>
2022-04-25 09:04:17 +0800

    add average function

--- read.py
+++ read.py
@@ -1,5 +1,7 @@
 data = []
 count = 0
+sum_length = 0
+average_length = 0
 try:
 
     with open('reviews.txt','r') as file:
@@ -7,7 +9,15 @@ try:
             count += 1
             data.append(line)
             if count % 1000 == 0:
-                print('----------%d----------'%count)
-                print(data[count - 1])
+                print(len(data))
+                # print('----------%d----------'%count)
+                # print(data[count - 1])
 except Exception as e:
     print(e)
+print('檔案讀取完了，總共有',len(data),'筆資料。')
+
+for item in data:
+    sum_length += len(item)
+average_length = sum_length / len(data)
+print('資料的平均長度為',average_length)
+
